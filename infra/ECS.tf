ca379bc1419517b370e9a8c2f22d6f1d59829b45
JulianaAmoasei <late.parrot@gmail.com>
2023-03-22 12:35:14 -0300

    fix: docker tag

--- infra/ECS.tf
+++ infra/ECS.tf
@@ -31,7 +31,7 @@ resource "aws_ecs_task_definition" "serverless2-API" {
     [
       {
         "name"      = "producao"
-        "image"     = "aquijuz/curso-serverless:v1"
+        "image"     = "aquijuz/curso-serverless:v2"
         "cpu"       = 256
         "memory"    = 512
         "essential" = true
